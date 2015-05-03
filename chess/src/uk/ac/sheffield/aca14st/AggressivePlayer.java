208aee6b7f8947ee0bcb71f3438f730ec612910b
Simon Turner <sturner4@sheffield.ac.uk>
2015-05-03 18:16:02 +0100

    Started work on AIs

--- chess/src/uk/ac/sheffield/aca14st/AggressivePlayer.java
+++ chess/src/uk/ac/sheffield/aca14st/AggressivePlayer.java
@@ -1,8 +1,19 @@
 package uk.ac.sheffield.aca14st;
 
-public class AggressivePlayer extends AIPlayer{
+public class AggressivePlayer extends Player{
 
     public AggressivePlayer(String name, Pieces pieces, Board board, Player opponent){
         super(name, pieces, board, opponent);
+    }
+
+    @Override
+    public boolean makeMove(){
+        boolean kingTaken = false;
+        //Iterate through all the pieces owned by this player.
+            //for each of those pieces, check every valid move
+                //if any of the valid moves are occupied by an enemy piece, check the value of that piece
+                    //move the piece that can take the most valuable piece
+        
+        return kingTaken;
     }
 }
--- chess/src/uk/ac/sheffield/aca14st/RandomPlayer.java
+++ chess/src/uk/ac/sheffield/aca14st/RandomPlayer.java
@@ -1,6 +1,6 @@
 package uk.ac.sheffield.aca14st;
 
-public class RandomPlayer extends AIPlayer{
+public class RandomPlayer extends Player{
 
     public RandomPlayer(String name, Pieces pieces, Board board, Player opponent){
         super(name, pieces, board, opponent);
@@ -8,6 +8,18 @@ public class RandomPlayer extends AIPlayer{
 
     @Override
     public boolean makeMove(){
-        return false;
+        boolean kingTaken = false;
+
+        //Is there a way of finding all the positions of all the pieces of a particular colour?
+        //If so, pick a random square from that list of positions - this would make things a lot more efficient.
+
+        //Pick a random square
+        //If it has a piece on it, that's the right colour for this player,
+            //pick a random target square
+                //if the move (init square, target square) is legal, move the piece
+                //else continue picking random target squares for that piece
+        //else continue picking random squares
+
+        return kingTaken;
     }
 }
